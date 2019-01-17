1448fca0c4bd6e72c2e8f6e5d358410923f9b018
Andre Piantino <piantino@gmail.com>
2019-01-17 15:06:54 +0000

    Criado teste para o MVP funcional

---
+++ mvp/crawler.py
@@ -0,0 +1,38 @@
+import time
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+from selenium import webdriver
+
+class Crawler(object):
+    def __init__(self):
+        self.browser = requests.Session()
+        
+    def get_acordaos(self, num_proc):
+        url = "http://busca.tjsc.jus.br/jurisprudencia/buscaajax.do?&nuProcesso=" + num_proc +"&categoria=acordaos&q=&only_ementa=&qualquer=&excluir=&prox1=&prox2=&proxc=&frase=&classe=&juizProlator=&origem=&relator=&radio_campo=ementa&faceta=true&busca=avancada&datainicial=&datafinal=&ps=20&sort=dtJulgamento+desc"
+
+        r = self.browser.get(url)
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        resultados = soup.find_all('div', class_="resultados")
+
+        acordaos = []
+        for resultado in resultados:
+            acordao = {
+                "texto": resultado.p.get_text().strip().replace('\n\n', '\n'),
+                "ementa": resultado.find_all('textarea')[0].find_all(text=True)[0].strip()
+            }
+
+            acordaos.append(acordao)
+        return acordaos
+
+
+if __name__ == "__main__":
+    crawler = Crawler()
+    acordaos = crawler.get_acordaos('0003960-47.2013.8.24.0039')
+
+    for acordao in acordaos:
+        print(acordao['texto'])
+        print(acordao['ementa'])
---
+++ mvp/limpeza.py
@@ -0,0 +1,39 @@
+import string
+import re
+from nltk import tokenize
+from unidecode import unidecode
+from stop_words import get_stop_words
+
+class Limpeza(object):  
+
+    def __init__(self):
+        self.remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+        self.stop_words = get_stop_words("pt")
+            
+
+    def tratar_texto(self, texto):
+        texto = texto.lower()
+        texto = texto.replace('<br>', '')
+        return texto
+
+    def remover_pontuacao(self, texto):
+        return texto.translate(self.remove_punctuation_map)
+
+    def remover_stop_words(self, texto):
+        words = [word for word in texto.split() if len(word) > 1 and word not in self.stop_words]
+        return ' '.join(words)
+
+    def normalizar(self, texto):
+        texto = self.tratar_texto(texto)
+        texto = self.remover_pontuacao(texto)
+            
+        # quebra em palavras
+        texto = self.remover_stop_words(texto)
+        
+        # remover acentuação
+        texto = unidecode(texto)
+        
+        #removendo números
+        texto = re.sub('[^a-zA-Z ]+', '', texto)
+
+        return texto
